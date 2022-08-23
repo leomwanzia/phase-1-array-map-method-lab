2b642d5ffbeddb7edf12fe621c15fa9e03231cf0
Leo <leomutungi19@gmail.com>
2022-08-23 20:33:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((element) =>
+      element
+        .split(" ")
+        .map((element) => element.charAt(0).toUpperCase() + element.slice(1))
+        .join(" ")
+  );
 }
